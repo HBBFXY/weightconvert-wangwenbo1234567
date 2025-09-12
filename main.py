619129566898955853ede13b8aee237d22a7df33
wangwenbo1234567 <a2112647901@163.com>
2025-09-12 10:52:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+#weightConvert
+W = input('请输入需转换的重量；')
+if W[-1] in ['G','g']:
+    W = eval(W[0:-2])*2.2046
+    print('转换后的重量为%.3f'%W)
+elif W[-1] in ['D','d']:
+    W = eval(W[0:-2])/2.2046
+    print('转换后的重量为%.3f'%W)
+else:
+    print('请输入正确的单位')
