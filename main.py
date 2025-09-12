9e0038a1682cc500630bcf3d76c5b3fbc438414a
wangwenbo1234567 <a2112647901@163.com>
2025-09-12 10:55:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
 W = input('请输入需转换的重量；')
-if W[-1] in ['G','g']:
+if W[-1] in ['g']:
     W = eval(W[0:-2])*2.2046
     print('转换后的重量为%.3f'%W)
-elif W[-1] in ['D','d']:
+elif W[-1] in ['d']:
     W = eval(W[0:-2])/2.2046
     print('转换后的重量为%.3f'%W)
 else:
