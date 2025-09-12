3b34b7364e424062512a84f2a594e3c8ca6d9e37
wangwenbo1234567 <a2112647901@163.com>
2025-09-12 11:24:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ if W[-1] in ['g']:
     print('转换后的重量为%.3f pd'%D)
 elif W[-1] in ['d']:
     G = eval(W[0:-2])/22.046
-    print('转换后的重量为%.3f kg'%G)
+    K = int(G*1000)
+    k = float(K/1000)
+    print('转换后的重量为%.3f kg'%k)
 else:
     print('请输入正确的单位')
